bc495ad505c02da100940af4c5ace547cf6e14a0
h4sh3d <h4sh3d@protonmail.com>
2021-02-22 18:23:50 +0100

    Change daemon session to generic

--- src/session/daemon.rs
+++ src/session/daemon.rs
@@ -5,24 +5,37 @@
 use monero::util::key::{PrivateKey, PublicKey as MPublicKey};
 use secp256k1::key::PublicKey;
 
-pub struct AliceSessionParameters {
-    pub destination_address: String,
-    pub buy: PublicKey,
-    pub cancel: PublicKey,
-    pub refund: PublicKey,
-    pub punish: PublicKey,
-    pub spend: MPublicKey,
+use crate::crypto::{Crypto, CryptoEngine};
+use crate::roles::{Accordant, Arbitrating};
+
+pub struct AliceSessionParameters<Ar, Ac, C>
+where
+    Ar: Arbitrating + Crypto<C>,
+    Ac: Accordant,
+    C: CryptoEngine,
+{
+    pub destination_address: Ar::Address,
+    pub buy: Ar::PublicKey,
+    pub cancel: Ar::PublicKey,
+    pub refund: Ar::PublicKey,
+    pub punish: Ar::PublicKey,
+    pub spend: Ac::PublicKey,
     /// The monero view key is the only secret key known by the daemon.
-    pub view: PrivateKey,
+    pub view: Ac::PrivateKey,
 }
 
-pub struct BobSessionParameters {
-    pub refund_address: String,
-    pub fund: PublicKey,
-    pub buy: PublicKey,
-    pub cancel: PublicKey,
-    pub refund: PublicKey,
-    pub spend: MPublicKey,
+pub struct BobSessionParameters<Ar, Ac, C>
+where
+    Ar: Arbitrating + Crypto<C>,
+    Ac: Accordant,
+    C: CryptoEngine,
+{
+    pub refund_address: Ar::Address,
+    pub fund: Ar::PublicKey,
+    pub buy: Ar::PublicKey,
+    pub cancel: Ar::PublicKey,
+    pub refund: Ar::PublicKey,
+    pub spend: Ac::PublicKey,
     /// The monero view key is the only secret key known by the daemon.
-    pub view: PrivateKey,
+    pub view: Ac::PrivateKey,
 }

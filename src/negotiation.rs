341571e3d1f335dcba275898e736505ecb1f539c
h4sh3d <h4sh3d@protonmail.com>
2021-02-27 18:51:14 +0100

    Add doc on offer, use broken inet2 crate for node address

--- src/negotiation.rs
+++ src/negotiation.rs
@@ -1,10 +1,14 @@
 //! Negotiation phase utilities
 
-use inet2_addr::InetSocketAddrExt;
+//use internet2::session::node_addr::NodeAddr;
 
 use crate::blockchains::{Fee, FeeStrategy};
 use crate::roles::{Accordant, Arbitrating, Network, SwapRole};
 
+/// An offer is created by a Maker before the start of his daemon, it references all the data
+/// needed to know what the trade look likes from a Taker perspective. The daemon start when the
+/// Maker is ready to finalyze his offer, transforming the offer into a public offer which contains
+/// the data needed to a Taker to connect to the Maker's daemon.
 pub struct Offer<Ar, Ac, S, N>
 where
     Ar: Arbitrating + Fee<S>,
@@ -40,7 +44,7 @@ where
     N: Network,
 {
     pub offer: Offer<Ar, Ac, S, N>,
-    pub daemon_service: InetSocketAddrExt,
+    //pub daemon_service: NodeAddr,
 }
 
 #[cfg(test)]

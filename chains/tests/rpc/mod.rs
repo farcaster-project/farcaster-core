df9f50cb436a49a8e2405c769d4953810184851c
h4sh3d <h4sh3d@protonmail.com>
2021-03-31 20:01:32 +0200

    Improve rpc tests

--- chains/tests/rpc/mod.rs
+++ chains/tests/rpc/mod.rs
@@ -1,63 +1,41 @@
 use bitcoincore_rpc::{Auth, Client};
 
-use bitcoin::secp256k1::{All, Secp256k1};
-use bitcoin::util::key::{PrivateKey, PublicKey};
-
-pub fn setup() -> Result<Client, bitcoincore_rpc::Error> {
-    Client::new(
-        "http://127.0.0.1:18443".into(),
-        Auth::UserPass(
-            "test".into(),
-            "cEl2o3tHHgzYeuu3CiiZ2FjdgSiw9wNeMFzoNbFmx9k=".into(),
-        ),
-    )
-}
-
-pub fn keys() -> (
-    Secp256k1<All>,
-    PrivateKey,
-    PublicKey,
-    PrivateKey,
-    PublicKey,
-    PrivateKey,
-    PublicKey,
-    PrivateKey,
-    PublicKey,
-) {
-    let secp = Secp256k1::new();
-
-    let privkey_a: PrivateKey =
-        PrivateKey::from_wif("L1HKVVLHXiUhecWnwFYF6L3shkf1E12HUmuZTESvBXUdx3yqVP1D").unwrap();
-    let pubkey_a = PublicKey::from_private_key(&secp, &privkey_a);
-
-    let privkey_b: PrivateKey =
-        PrivateKey::from_wif("Kwe6eTweXYHsWX1yazBBCqB2eBSnqK6m8BPBvpJmv5pmuWi8nx1w").unwrap();
-    let pubkey_b = PublicKey::from_private_key(&secp, &privkey_b);
-
-    let privkey_a2: PrivateKey =
-        PrivateKey::from_wif("Kx9uWX33oa5TbCc7Mo7vCM7yN75mehEw9aSkAeqJdC2kd1YKuKXR").unwrap();
-    let pubkey_a2 = PublicKey::from_private_key(&secp, &privkey_a2);
-
-    let privkey_b2: PrivateKey =
-        PrivateKey::from_wif("L3ienZ4Zg1EP2HiiqsWih1Wkr3yuKJwTV5svqGMry1dYdrXQED8Q").unwrap();
-    let pubkey_b2 = PublicKey::from_private_key(&secp, &privkey_b2);
-
-    (
-        secp, privkey_a, pubkey_a, privkey_b, pubkey_b, privkey_a2, pubkey_a2, privkey_b2,
-        pubkey_b2,
-    )
+lazy_static::lazy_static! {
+    pub static ref CLIENT: Client =  {
+        Client::new(
+            "http://127.0.0.1:18443".into(),
+            Auth::UserPass(
+                "test".into(),
+                "cEl2o3tHHgzYeuu3CiiZ2FjdgSiw9wNeMFzoNbFmx9k=".into(),
+            ),
+        ).unwrap()
+    };
 }
 
 macro_rules! gen_to_add {
-    ($num:expr => $add:ident with $client:ident) => (
-        $client.generate_to_address($num, &$add).unwrap()
+    ($num:expr => $add:expr) => (
+        rpc::CLIENT.generate_to_address($num, &$add).unwrap()
     );
-    ($add:ident with $client:ident) => (
-        gen_to_add!(1 => $add with $client)
+    ($add:expr) => (
+        gen_to_add!(1 => $add)
     );
 }
 
-macro_rules! gen_new_add {
+macro_rules! get_block {
+    ($block:expr) => {
+        rpc::CLIENT.get_block(&$block).unwrap()
+    };
+}
+
+macro_rules! fund_address {
+    ($add:expr) => {{
+        let blocks = gen_to_add!($add);
+        let block = get_block!(blocks[0]);
+        block.coinbase().unwrap().clone()
+    }};
+}
+
+macro_rules! new_address {
     () => {{
         use bitcoin::network::constants::Network;
         use bitcoin::secp256k1::rand::thread_rng;
@@ -67,18 +45,52 @@ macro_rules! gen_new_add {
 
         // Generate random key pair
         let s = Secp256k1::new();
+        let pair = s.generate_keypair(&mut thread_rng());
         let public_key = key::PublicKey {
             compressed: true,
-            key: s.generate_keypair(&mut thread_rng()).1,
+            key: pair.1,
+        };
+        let private_key = key::PrivateKey {
+            compressed: true,
+            network: Network::Regtest,
+            key: pair.0,
         };
 
         // Generate pay-to-pubkey-hash address
-        Address::p2pkh(&public_key, Network::Regtest)
+        (Address::p2pkh(&public_key, Network::Regtest), public_key, private_key)
     }};
 }
 
 macro_rules! send {
-    ($tx:ident with $client:ident) => {
-        $client.send_raw_transaction(&$tx).unwrap()
+    ($tx:expr) => {
+        rpc::CLIENT.send_raw_transaction(&$tx).unwrap()
+    };
+}
+
+macro_rules! mine {
+    () => {{
+        mine!(1)
+    }};
+
+    ($num:literal) => {{
+        gen_to_add!($num => new_address!().0)
+    }};
+}
+
+macro_rules! rpc {
+    () => {};
+
+    ( $(then)? mine $nb:expr; $($tail:tt)* ) => {
+        {
+            mine!($nb);
+            rpc!($($tail)*);
+        }
+    };
+
+    ( $(then)? broadcast $tx:expr; $($tail:tt)* ) => {
+        {
+            send!($tx);
+            rpc!($($tail)*);
+        }
     };
 }
--- chains/tests/transactions.rs
+++ chains/tests/transactions.rs
@@ -13,93 +13,125 @@ use bitcoincore_rpc::RpcApi;
 #[macro_use]
 mod rpc;
 
+macro_rules! setup_txs {
+    () => {{
+        let (_, pubkey_a1, secret_a1) = new_address!();
+        let (_, pubkey_a2, secret_a2) = new_address!();
+
+        let (_, pubkey_b1, _secret_b1) = new_address!();
+        let (_, pubkey_b2,  secret_b2) = new_address!();
+
+        let mut funding = Funding::initialize(pubkey_a1, Network::Local).unwrap();
+        let address = funding.get_address().unwrap();
+
+        let funding_tx_seen = fund_address!(address);
+
+        funding.update(funding_tx_seen).unwrap();
+
+        let datalock = DataLock {
+            timelock: CSVTimelock::new(10),
+            success: DoubleKeys::new(pubkey_a1, pubkey_b1),
+            failure: DoubleKeys::new(pubkey_a2, pubkey_b2),
+        };
+
+        let fee = FeeStrategy::Fixed(SatPerVByte::from_sat(1));
+        let politic = FeePolitic::Aggressive;
+
+        let mut lock = Tx::<Lock>::initialize(&funding, datalock.clone(), &fee, politic).unwrap();
+
+        //
+        // Create cancel tx
+        //
+        let datapunishablelock = DataPunishableLock {
+            timelock: CSVTimelock::new(10),
+            success: DoubleKeys::new(pubkey_a1, pubkey_b1),
+            failure: pubkey_a2,
+        };
+
+        let mut cancel =
+            Tx::<Cancel>::initialize(&lock, datalock, datapunishablelock.clone(), &fee, politic)
+                .unwrap();
+
+        //
+        // Create refund tx
+        //
+        let (new_address, _, _) = new_address!();
+        let refund =
+            Tx::<Refund>::initialize(&cancel, datapunishablelock, new_address, &fee, politic).unwrap();
+
+        //
+        // Co-Sign refund
+        //
+        let _sig = cancel.generate_failure_witness(&secret_a2).unwrap();
+        let _sig = cancel.generate_failure_witness(&secret_b2).unwrap();
+
+        //
+        // Finalize for failure path
+        //
+        cancel.finalize().unwrap();
+        let cancel_finalized = cancel.extract();
+
+        //
+        // Sign lock tx
+        //
+        let _sig = lock.generate_witness(&secret_a1).unwrap();
+        let lock_finalized = lock.extract();
+
+        (lock_finalized, cancel_finalized, refund)
+    }}
+}
+
 #[test]
-fn create_funding_generic() {
-    let client = rpc::setup().unwrap();
+fn create_transactions() {
+    setup_txs!();
+}
 
-    let (
-        _secp,
-        privkey_a,
-        pubkey_a,
-        _privkey_b,
-        pubkey_b,
-        privkey_a2,
-        pubkey_a2,
-        privkey_b2,
-        pubkey_b2,
-    ) = rpc::keys();
+#[test]
+fn broadcast_lock() {
 
-    let mut funding = Funding::initialize(pubkey_a, Network::Local).unwrap();
-    let address = funding.get_address().unwrap();
+    let (lock_finalized, _, _) = setup_txs!();
 
-    let blocks = gen_to_add!(address with client);
+    rpc! {
+        // Wait 100 blocks to unlock the coinbase
+        mine 100;
 
-    let block_hash = blocks[0];
-    let block = client.get_block(&block_hash).unwrap();
-    let funding_tx_seen = block.coinbase().unwrap().clone();
+        // Broadcast the lock and mine the number of blocks needed for CSV
+        then broadcast lock_finalized;
+        then mine 1;
+    }
+}
 
-    funding.update(funding_tx_seen).unwrap();
+#[test]
+#[should_panic]
+fn broadcast_cancel_before_timelock() {
 
-    let datalock = DataLock {
-        timelock: CSVTimelock::new(10),
-        success: DoubleKeys::new(pubkey_a, pubkey_b),
-        failure: DoubleKeys::new(pubkey_a2, pubkey_b2),
-    };
+    let (lock_finalized, cancel_finalized, _) = setup_txs!();
 
-    let fee = FeeStrategy::Fixed(SatPerVByte::from_sat(50));
-    let politic = FeePolitic::Aggressive;
+    rpc! {
+        // Wait 100 blocks to unlock the coinbase
+        mine 100;
 
-    let mut lock = Tx::<Lock>::initialize(&funding, datalock.clone(), &fee, politic).unwrap();
+        // Broadcast the lock and directly cancel without waiting the lock
+        then broadcast lock_finalized;
+        then broadcast cancel_finalized;
+    }
+}
 
-    //
-    // Create cancel tx
-    //
-    let datapunishablelock = DataPunishableLock {
-        timelock: CSVTimelock::new(10),
-        success: DoubleKeys::new(pubkey_a, pubkey_b),
-        failure: pubkey_a2,
-    };
+#[test]
+fn broadcast_cancel_after_timelock() {
 
-    let mut cancel =
-        Tx::<Cancel>::initialize(&lock, datalock, datapunishablelock.clone(), &fee, politic)
-            .unwrap();
+    let (lock_finalized, cancel_finalized, _) = setup_txs!();
 
-    //
-    // Create refund tx
-    //
-    let new_address = gen_new_add!();
-    let _refund =
-        Tx::<Refund>::initialize(&cancel, datapunishablelock, new_address, &fee, politic).unwrap();
+    rpc! {
+        // Wait 100 blocks to unlock the coinbase
+        mine 100;
 
-    //
-    // Co-Sign refund
-    //
-    let _sig = cancel.generate_failure_witness(&privkey_a2).unwrap();
-    let _sig = cancel.generate_failure_witness(&privkey_b2).unwrap();
+        // Broadcast the lock and mine the number of blocks needed for CSV
+        then broadcast lock_finalized;
+        then mine 10;
 
-    //
-    // Finalize for failure path
-    //
-    cancel.finalize().unwrap();
-    let cancel_finalized = cancel.extract();
-
-    //
-    // Sign lock tx
-    //
-    let _sig = lock.generate_witness(&privkey_a).unwrap();
-    let lock_finalized = lock.extract();
-
-    gen_to_add!(100 => address with client);
-
-    // Broadcast the lock
-    send!(lock_finalized with client);
-    // Mine the transaction
-    gen_to_add!(10 => address with client);
-
-    // Broadcast the cancel
-    send!(cancel_finalized with client);
-    // Mine the transaction
-    gen_to_add!(address with client);
-
-    assert!(true);
+        // Broadcast the cancel and mine the transaction
+        then broadcast cancel_finalized;
+        then mine 1;
+    }
 }

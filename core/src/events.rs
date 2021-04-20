a50c42aa8dc26bc13083888b0bb3b386dfb4c1ea
Luke Parker <lukeparker5132@gmail.com>
2021-04-20 17:23:44 -0500

    Have Syncer methods return Result; add poll method and Event enum.

--- core/src/events.rs
+++ core/src/events.rs
@@ -1,1 +1,24 @@
-pub enum Event {}
+use enum_dispatch::enum_dispatch;
+
+pub trait EventCore {
+    fn id(&self) -> i32;
+}
+
+#[derive(Debug, Clone)]
+pub struct HeightChanged {
+    pub id: i32,
+    pub block: Vec<u8>,
+    pub height: u64,
+}
+
+impl EventCore for HeightChanged {
+    fn id(&self) -> i32 {
+        self.id
+    }
+}
+
+#[enum_dispatch(EventCore)]
+#[derive(Debug, Clone)]
+pub enum Event {
+  HeightChanged,
+}

04ab24b72c7980a2700c237db16340844ef8ebc6
h4sh3d <h4sh3d@protonmail.com>
2021-07-13 17:27:55 +0200

    Debug, THIS WILL DISPLAY THE SECRET IN LOG, change it after run

--- run-rpc-tests.sh
+++ run-rpc-tests.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-ID=$(docker run -d -p 18443:18443 coblox/bitcoin-core\
+ID=$(docker run --rm -d -p 18443:18443 coblox/bitcoin-core\
     -regtest\
-    -server=1\
+    -server\
     -rpcbind=0.0.0.0\
      -rpcallowip=0.0.0.0/0\
     -rpcuser=test\
     -rpcpassword=cEl2o3tHHgzYeuu3CiiZ2FjdgSiw9wNeMFzoNbFmx9k=)
 
-RPC_HOST=127.0.0.1 RPC_PORT=18443 RPC_USER=test RPC_PASS=cEl2o3tHHgzYeuu3CiiZ2FjdgSiw9wNeMFzoNbFmx9k= cargo test --test transactions --features rpc -- --test-threads=1
+RPC_HOST=127.0.0.1 RPC_PORT=18443 RPC_USER=ci RPC_PASS=cEl2o3tHHgzYeuu3CiiZ2FjdgSiw9wNeMFzoNbFmx9k= cargo test --test transactions --features rpc -- --test-threads=1 --nocapture
+
 
 docker kill $ID

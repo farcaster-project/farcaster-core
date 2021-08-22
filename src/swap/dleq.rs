6a061dfa37265ef9dc5e3192129bc8cb4d35bb3f
Robert Hambrock <roberthambrock@gmail.com>
2021-08-22 12:25:27 +0200

    alias for tidyness

--- src/swap/dleq.rs
+++ src/swap/dleq.rs
@@ -1,19 +1,21 @@
-use curve25519_dalek;
+use curve25519_dalek::{edwards::EdwardsPoint as ed25519Point, scalar::Scalar as ed25519Scalar};
 
 #[cfg(feature = "experimental")]
-use ecdsa_fun;
+use ecdsa_fun::fun::{Point as secp256k1Point, Scalar as secp256k1Scalar};
+
+use crate::consensus::CanonicalBytes;
 
 struct DLEQProof {
-    xg_p: curve25519_dalek::edwards::EdwardsPoint,
-    xh_p: ecdsa_fun::fun::Point,
-    c_g: Vec<curve25519_dalek::edwards::EdwardsPoint>,
-    c_h: Vec<ecdsa_fun::fun::Point>,
-    e_g_0: curve25519_dalek::scalar::Scalar,
-    e_h_0: ecdsa_fun::fun::Scalar,
-    e_g_1: curve25519_dalek::scalar::Scalar,
-    e_h_1: ecdsa_fun::fun::Scalar,
-    a_0: curve25519_dalek::scalar::Scalar,
-    a_1: ecdsa_fun::fun::Scalar,
-    b_0: curve25519_dalek::scalar::Scalar,
-    b_1: ecdsa_fun::fun::Scalar,
+    xg_p: ed25519Point,
+    xh_p: secp256k1Point,
+    c_g: Vec<ed25519Point>,
+    c_h: Vec<secp256k1Point>,
+    e_g_0: Vec<ed25519Scalar>,
+    e_h_0: Vec<secp256k1Scalar>,
+    e_g_1: Vec<ed25519Scalar>,
+    e_h_1: Vec<secp256k1Scalar>,
+    a_0: Vec<ed25519Scalar>,
+    a_1: Vec<secp256k1Scalar>,
+    b_0: Vec<ed25519Scalar>,
+    b_1: Vec<secp256k1Scalar>,
 }

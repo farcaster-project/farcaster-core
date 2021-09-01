0dda10a541f2cb2a3c885923caf361420f9a4891
Robert Hambrock <roberthambrock@gmail.com>
2021-09-01 02:18:13 +0200

    dynamic highest bit committed to
    also, fix commitment to highest bit: was not multiplying highest bit's value by
    the highest bit's power of two in commitment

--- src/swap/dleq.rs
+++ src/swap/dleq.rs
@@ -62,13 +62,17 @@ impl From<(bool, usize)> for PedersenCommitment<ed25519Point, ed25519Scalar> {
     }
 }
 
-impl From<(bool, ed25519Scalar)> for PedersenCommitment<ed25519Point, ed25519Scalar> {
+impl From<(bool, usize, ed25519Scalar)> for PedersenCommitment<ed25519Point, ed25519Scalar> {
     fn from(
-        (bit, blinder): (bool, ed25519Scalar),
+        (bit, index, blinder): (bool, usize, ed25519Scalar),
     ) -> PedersenCommitment<ed25519Point, ed25519Scalar> {
+
+        let one: u256 = u256::from(1u32);
+        let order = one << index;
+
         let commitment = match bit {
             false => blinder * G,
-            true => G_p() + blinder * G,
+            true => G_p() * ed25519Scalar::from_bits(order.to_le_bytes()) + blinder * G,
         };
 
         PedersenCommitment {
@@ -107,15 +111,15 @@ impl From<(bool, ed25519Scalar)> for PedersenCommitment<secp256k1Point, secp256k
     }
 }
 
-fn key_commitment(x: [u8; 32]) -> Vec<PedersenCommitment<ed25519Point, ed25519Scalar>> {
+fn key_commitment(x: [u8; 32], msb_index: usize) -> Vec<PedersenCommitment<ed25519Point, ed25519Scalar>> {
     let x_bits = bitvec::prelude::BitSlice::<bitvec::order::Lsb0, u8>::from_slice(&x).unwrap();
     let mut commitment: Vec<PedersenCommitment<ed25519Point, ed25519Scalar>> = x_bits
         .iter()
-        .take(x_bits.len() - 1)
+        .take(msb_index)
         .enumerate()
         .map(|(index, bit)| (*bit, index).into())
         .collect();
-    let commitment_last = x_bits.get(x_bits.len() - 1).unwrap();
+    let commitment_last = x_bits.get(msb_index).unwrap();
     let commitment_last_value = match *commitment_last {
         true => ed25519Scalar::one(),
         false => ed25519Scalar::zero(),
@@ -123,7 +127,7 @@ fn key_commitment(x: [u8; 32]) -> Vec<PedersenCommitment<ed25519Point, ed25519Sc
     let blinder_last = commitment
         .iter()
         .fold(ed25519Scalar::zero(), |acc, x| acc - x.blinder);
-    commitment.push((*commitment_last, blinder_last).into());
+    commitment.push((*commitment_last, msb_index, blinder_last).into());
     commitment
 }
 
@@ -146,6 +150,7 @@ impl DLEQProof {
     fn generate(x: [u8; 32]) -> Self {
         let x_ed25519 = ed25519Scalar::from_bytes_mod_order(x);
         let xg_p = x_ed25519 * G_p();
+        let highest_bit = 255;
 
         // TODO: do properly
         let mut x_secp256k1: secp256k1Scalar<_> = secp256k1Scalar::from_bytes(x)
@@ -158,7 +163,7 @@ impl DLEQProof {
         DLEQProof {
             xg_p,
             xh_p,
-            c_g: key_commitment(x),
+            c_g: key_commitment(x, highest_bit),
             c_h: vec![PedersenCommitment::<secp256k1Point, secp256k1Scalar>::default()],
             e_g_0: vec![ed25519Scalar::default()],
             e_h_0: vec![secp256k1Scalar::random(&mut rand::thread_rng())],
@@ -174,8 +179,10 @@ impl DLEQProof {
 
 #[test]
 fn pedersen_commitment_works() {
-    let x: [u8; 32] = rand::thread_rng().gen();
-    let key_commitment = key_commitment(x);
+    let mut x: [u8; 32] = rand::thread_rng().gen();
+    // ensure 256th bit is 0
+    x[31] &= 0b0111_1111;
+    let key_commitment = key_commitment(x, 254);
     let commitment_acc = key_commitment
         .iter()
         .fold(ed25519Point::identity(), |acc, bit_commitment| {
@@ -190,7 +197,7 @@ fn pedersen_commitment_works() {
 #[test]
 fn blinders_sum_to_zero() {
     let x: [u8; 32] = rand::thread_rng().gen();
-    let key_commitment = key_commitment(x);
+    let key_commitment = key_commitment(x, 254);
     let blinder_acc = key_commitment
         .iter()
         .fold(ed25519Scalar::zero(), |acc, bit_commitment| {

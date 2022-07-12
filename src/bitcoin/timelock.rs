c5b79ce06463e29065978c682a5c9c7c5c97554d
h4sh3d <h4sh3d@protonmail.com>
2022-07-12 11:23:11 +0200

    feat: impl timelock from (#265)
    * style: move type at the top of the file
    
    * feat: add from impls for timelock

--- src/bitcoin/timelock.rs
+++ src/bitcoin/timelock.rs
@@ -5,6 +5,11 @@ use crate::consensus::{self, CanonicalBytes};
 use std::fmt::Debug;
 use std::str::FromStr;
 
+/// An `OP_CSV` value (32-bits integer) to use in transactions and scripts.
+#[derive(PartialEq, Eq, PartialOrd, Clone, Debug, Copy, Display, Serialize, Deserialize)]
+#[display("{0} blocks")]
+pub struct CSVTimelock(u32);
+
 impl FromStr for CSVTimelock {
     type Err = consensus::Error;
 
@@ -15,11 +20,6 @@ impl FromStr for CSVTimelock {
         Ok(CSVTimelock(x))
     }
 }
-
-/// An `OP_CSV` value (32-bits integer) to use in transactions and scripts.
-#[derive(PartialEq, Eq, PartialOrd, Clone, Debug, Copy, Display, Serialize, Deserialize)]
-#[display("{0} blocks")]
-pub struct CSVTimelock(u32);
 
 impl CSVTimelock {
     /// Create a new raw check sequence verify timelock of given value.
@@ -35,6 +35,30 @@ impl CSVTimelock {
     /// Return the value of nSequence that disable `CHECK_SEQUENCE_VERIFY`.
     pub fn disable() -> u32 {
         (1 << 31) as u32
+    }
+}
+
+impl From<u32> for CSVTimelock {
+    fn from(u: u32) -> Self {
+        Self::new(u)
+    }
+}
+
+impl From<CSVTimelock> for u32 {
+    fn from(ti: CSVTimelock) -> Self {
+        ti.as_u32()
+    }
+}
+
+impl From<u16> for CSVTimelock {
+    fn from(u: u16) -> Self {
+        Self::new(u as u32)
+    }
+}
+
+impl From<u8> for CSVTimelock {
+    fn from(u: u8) -> Self {
+        Self::new(u as u32)
     }
 }
 
